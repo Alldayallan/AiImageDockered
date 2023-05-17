ce4aa408afacb2342e9d571549ed7d5b7a049b72
Allan Reid <mail@allanreid.co>
2023-05-16 20:13:25 -0700

    added code to formfield and createpost

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,14 +1,24 @@
 import React, { useState, useEffect } from 'react';
 
 import { Loader, Card, FormField } from '../components';
-import {Form} from "react-router-dom";
+
+const RenderCards = ({ data, title }) => {
+    if(data?.length > 0)  {
+        return (
+            data.map((post) => <Card key={post._id} {...post} />)
+        );
+    }
+
+    return (
+        <h2 className="mt-5 font-bold text-[#6469ff] text-xl uppercase">{title}</h2>
+    );
+};
 
 const Home = () => {
     const [loading, setLoading] = useState(false);
     const [allPosts, setAllPosts] = useState(null);
 
     const [searchText, setSearchText] = useState('');
-
 
     return (
         <section className="max-w-7xl mx-auto">
@@ -34,7 +44,17 @@ const Home = () => {
                             </h2>
                         )}
                         <div className="grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3">
-
+                            {searchText ? (
+                                <RenderCards
+                                    data={[]}
+                                    title="No Search Results Found" 
+                                    />
+                            ) : (
+                                <RenderCards
+                                data={[]}
+                                title="No Posts Yet"
+                                />
+                            )}
                         </div>
                     </>
                 )}
